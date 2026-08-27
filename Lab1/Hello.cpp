f50b7217f711f170d05dc5319ed5ecc1d1d1c6e1
gbajwa05 <gbajwa@cpp.edu>
2026-08-27 16:41:06 -0700

    Initial commit

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/**
+ * Prints Hello World using C++ output statements.
+**/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
